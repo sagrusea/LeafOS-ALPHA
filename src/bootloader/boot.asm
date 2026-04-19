a9449f7e60fabdc6d7eb8fb30d8e7cfdf54121fc
sagru <145695175+sagrusea@users.noreply.github.com>
2026-04-19 15:38:38 +0200

    Initialize 16-bit mode and stack
    Add [bits 16] and initialize segment registers and stack (xor ax,ax; mov ds,es,ss = 0; mov bp/sp = 0x7C00) so the bootloader has a safe, deterministic stack before executing. Leaves BOOT_DRIVE, includes and boot signature intact; prevents unpredictable behavior from uninitialized segments/SP in early boot.

--- src/bootloader/boot.asm
+++ src/bootloader/boot.asm
@@ -1,4 +1,13 @@
 [org 0x7c00]
+[bits 16]
+; make sure stack is safe
+xor ax, ax
+mov ds, ax
+mov es, ax
+mov ss, ax
+mov bp, 0x7c00
+mov sp, bp
+
 mov [BOOT_DRIVE], dl
 
 mov si, msg
@@ -17,8 +26,8 @@ jmp 0x0000:0x7e00
 BOOT_DRIVE: db 0
 msg: db "LeafOS",0
 
-
 %include "./src/bootloader/print.asm"
 %include "./src/bootloader/disk.asm"
+
 times 510-($-$$) db 0
 dw 0xaa55
